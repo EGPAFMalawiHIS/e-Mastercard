dd5c115e665aed0680356da26d71222705cbd086
Martin Suleman <sulemartin87@gmail.com>
2020-02-07 13:28:49 +0200

    working on patient search

---
+++ src/components/PatientCard.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="card mb-3" style="max-width: 540px;">
+        <div class="row no-gutters">
+            <div class="col-md-4">
+                <img src="../assets/man.png" class="card-img rounded-circle" alt="..."  style="margin: auto;">
+            </div>
+            <div class="col-md-8">
+                <div class="card-body">
+                    
+                    <h5 class="card-title">  Martin Suleman <small>19 december 1999 (23)</small></h5>
+                    <p class="card-text">ARV Number: 10</p>
+                    <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
---
+++ src/views/Search.vue
@@ -0,0 +1,77 @@
+<template>
+  <div>
+    <div class="d-flex" id="wrapper">
+      <!-- Sidebar -->
+      <side-bar />
+      <!-- /#sidebar-wrapper -->
+
+      <!-- Page Content -->
+      <div id="page-content-wrapper">
+        <top-nav />
+        <div class="container-fluid">
+            	
+          <input class="form-control rounded border-primary" id="myInput" type="text" placeholder="Search for a patient.." v-model="search_text"/>
+          <br />
+          <table class="table table-bordered table-striped">
+            <thead>
+              <tr>
+                <th>Firstname</th>
+                <th>Lastname</th>
+                <th>Email</th>
+              </tr>
+            </thead>
+            <tbody id="myTable">
+              <tr>
+                <td>{{search_text}}</td>
+                <td>
+                  <patient-card/>
+                </td>
+                <td>john@example.com</td>
+              </tr>
+              <tr>
+                <td>Mary</td>
+                <td>Moe</td>
+                <td>mary@mail.com</td>
+              </tr>
+              <tr>
+                <td>July</td>
+                <td>Dooley</td>
+                <td>july@greatstuff.com</td>
+              </tr>
+              <tr>
+                <td>Anja</td>
+                <td>Ravendale</td>
+                <td>a_r@test.com</td>
+              </tr>
+            </tbody>
+          </table>
+        </div>
+      </div>
+      <!-- /#page-content-wrapper -->
+    </div>
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import TopNav from "@/components/topNav.vue";
+import Sidebar from "@/components/SideBar.vue";
+import PatientCard from "@/components/PatientCard.vue";
+
+export default {
+  name: "home",
+  components: {
+    "top-nav": TopNav,
+    "side-bar": Sidebar,
+    "patient-card": PatientCard
+  },
+  data: function() {
+      return {
+          search_text : null,
+      }
+  }
+};
+</script>
+
+<style scoped>
+</style>
