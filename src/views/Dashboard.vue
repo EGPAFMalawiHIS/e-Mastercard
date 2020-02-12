a905119ac456524cddd58e1e9e1fe0b02ed94179
Jmwantisi <jamesmwantisi@gmail.com>
2020-02-12 16:16:38 +0200

    Added dashboard routing component

---
+++ src/views/Dashboard.vue
@@ -0,0 +1,45 @@
+<template>
+  
+<div>
+    <div class="d-flex" id="wrapper">
+
+    <!-- Sidebar -->
+    <side-bar/>
+    <!-- /#sidebar-wrapper -->
+    
+    <!-- Page Content -->
+    <div id="page-content-wrapper">
+
+    <top-nav/>
+      <div class="container-fluid">
+        <dashboard style="margin-top:10px"/>
+      </div>
+    </div>
+    <!-- /#page-content-wrapper -->
+    
+  </div>
+</div>
+</template>
+
+<script>
+// @ is an alias to /src
+import TopNav from '@/components/topNav.vue';
+import Sidebar from '@/components/SideBar.vue'
+import PatientDashboard from '@/components/dashboard/PatientDashboard.vue'
+
+export default {
+  name: 'home',
+  components: {
+    "top-nav": TopNav,
+    "side-bar": Sidebar,
+    "dashboard": PatientDashboard
+  }
+}
+</script>
+
+<style >
+  .red{
+    background-color: red;
+  }
+
+</style>
