22317b086acf8f02606d1144a33965b6143bce00
Martin Suleman <sulemartin87@gmail.com>
2020-02-26 21:29:37 +0200

    working on HIV clinical registration

--- src/views/Mastercard.vue
+++ src/views/Mastercard.vue
@@ -20,6 +20,29 @@
          
        <visit-data></visit-data>
        </div>
+       <button type="button" class="btn btn-primary" data-toggle="modal" data-target="#exampleModalCenter">
+  Launch demo modal
+</button>
+       <div class="modal fade" id="exampleModalCenter" tabindex="-1" role="dialog" aria-labelledby="exampleModalCenterTitle" aria-hidden="true">
+        <div class="modal-dialog modal-dialog-centered" role="document">
+          <div class="modal-content">
+            <div class="modal-header">
+              <h5 class="modal-title" id="exampleModalLongTitle">Modal title</h5>
+              <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                <span aria-hidden="true">&times;</span>
+              </button>
+            </div>
+            <div class="modal-body">
+              <clinic-registration></clinic-registration>
+              <!-- ... -->
+            </div>
+            <div class="modal-footer">
+              <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+              <button type="button" class="btn btn-primary">Save changes</button>
+            </div>
+          </div>
+        </div>
+      </div>
       </div>
     </div>
     <!-- /#page-content-wrapper -->
@@ -34,6 +57,7 @@ import TopNav from '@/components/topNav.vue';
 import Sidebar from '@/components/SideBar.vue';
 import PatientInformation from '@/components/PatientInformation.vue';
 import VisitData from '@/components/VisitData.vue';
+import clinicRegistration from '@/components/encounters/clinicRegistration.vue'
 
 export default {
   name: 'home',
@@ -41,7 +65,8 @@ export default {
     "top-nav": TopNav,
     "side-bar": Sidebar,
     "patient-information": PatientInformation,
-    "visit-data": VisitData
+    "visit-data": VisitData,
+    "clinic-registration": clinicRegistration
   }
 }
 </script>
