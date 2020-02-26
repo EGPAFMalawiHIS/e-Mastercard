639aa2b80519099fd88d089d279f89504ec0b3c4
mwatha <mwathabwanali@gmail.com>
2020-02-26 13:53:18 +0200

    cohort report: 90%

--- src/views/CohortDriller.vue
+++ src/views/CohortDriller.vue
@@ -1,5 +1,42 @@
 <template>
-    <div>
-        ....
+  <div>
+    <div class="d-flex" id="wrapper">
+      <!-- Sidebar -->
+      <side-bar />
+      <!-- /#sidebar-wrapper -->
+
+      <!-- Page Content -->
+      <div id="page-content-wrapper">
+        <top-nav />
+        <!-- Page Content -->
+        
+
+        <div class="cohort">
+
+        </div>
+
+
+
+        <!-- Page Content end -->
+      </div>
     </div>
+
+  </div>
 </template>
+
+
+<script>
+import TopNav from "@/components/topNav.vue";
+import Sidebar from "@/components/SideBar.vue"
+
+export default {
+  name: "reports",
+  components: {
+    "top-nav": TopNav,
+    "side-bar": Sidebar
+  }
+
+
+}
+
+</script>
