baae8b30b115aab4e3ced44202cea991ae62c6e8
Martin Suleman <sulemartin87@gmail.com>
2020-03-03 21:39:36 +0200

    starting clinic consultation

---
+++ src/components/encounters/consultation.vue
@@ -0,0 +1,35 @@
+<template>
+  <div></div>
+</template>
+
+<script>
+export default {
+
+data: function() {
+    return {
+
+      // weight: {
+      //   value_numeric: null,
+      //   concept_id: 5090
+      // }
+    }
+},
+methods: {
+  saveEncounter: function() {
+    let encounterObject = {
+      vitals: {
+        encounter_id: 53,
+        obs: {
+          // weight: this.weight,
+        }
+      }
+    }
+    this.$emit('addEncounter',  encounterObject);
+  }
+}
+}
+</script>
+
+<style>
+
+</style>
