ac81d6cff944673731fbc4f2930c5afa77ce08c0
Walter Kaunda <walterkaunda@live.co.uk>
2020-03-09 16:43:47 +0200

    Added container for pages.

---
+++ src/components/PageView.vue
@@ -0,0 +1,29 @@
+<template>
+  <div class="d-flex" id="wrapper">
+    <SideBar />
+    
+    <div id="page-content-wrapper">
+      <TopNav />
+      <div class="content">
+        <slot></slot>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import SideBar from "./SideBar";
+import TopNav from "./topNav";
+
+export default {
+  name: 'PageView',
+  components: {SideBar, TopNav}
+}
+</script>
+
+<style scoped>
+div.content {
+  padding-left: 5%;
+  padding-right: 5%;
+}
+</style>
