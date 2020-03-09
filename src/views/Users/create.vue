2af57d63d12f79115f6ea5d7bfd069a370480f1d
Walter Kaunda <walterkaunda@live.co.uk>
2020-03-09 16:48:01 +0200

    Added user create form and page.

---
+++ src/views/Users/create.vue
@@ -0,0 +1,14 @@
+<template>
+  <PageView>
+    <UserCreateForm />
+  </PageView>
+</template>
+
+<script>
+import PageView from "@/components/PageView";
+import UserCreateForm from "@/components/users/UserCreateForm"
+
+export default {
+  components: {PageView, UserCreateForm}
+}
+</script>
