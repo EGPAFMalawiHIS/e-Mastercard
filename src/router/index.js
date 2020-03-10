49810c531512c39adeb603cb9831b7a89bb49808
Martin Suleman <sulemartin87@gmail.com>
2020-03-10 15:30:18 +0200

    added settings page

--- src/router/index.js
+++ src/router/index.js
@@ -122,6 +122,11 @@ const routes = [
     path: "/pepfar_defaulter_list",
     name: "PepfarDefaulterList",
     component: () => import("../views/PepfarDefaulterList.vue")
+  },
+  {
+    path: "/settings",
+    name: "Settings",
+    component: () => import("../views/Settings.vue")
   }
 ]
 
