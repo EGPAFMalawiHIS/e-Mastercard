d20d10a9563f3506ddde7554c264ccff642eed33
Walter Kaunda <walterkaunda@live.co.uk>
2020-03-12 21:04:53 +0200

    Added user edit page

---
+++ src/views/Users/edit.vue
@@ -0,0 +1,69 @@
+<template>
+  <PageView>
+    <UserEditForm @on-submit="editUser"
+                  :user="user"
+                  :editMode="true"
+                  submitText="Update user" />
+  </PageView>
+</template>
+
+<script>
+import ApiClient from "@/services/api_client";
+
+import PageView from "@/components/PageView";
+import UserEditForm from "@/components/users/UserEditForm"
+
+export default {
+  components: {PageView, UserEditForm},
+  created() {
+    this.fetchUser()
+        .then(user => {
+          const name = user.person.names[0];
+
+          this.user = {
+            user_id: user.user_id,
+            username: user.username,
+            given_name: name && name.given_name,
+            family_name: name && name.family_name,
+            roles: user.roles
+          }
+        });
+  },
+  computed: {
+    userId() {
+      return this.$route.params['userId'];
+    }
+  },
+  data() {
+    return {
+      user: {}
+    }
+  },
+  methods: {
+    async editUser(user) {
+      const response = await ApiClient.post('/users', user); 
+
+      if (response.status == 400) {
+        response.text()
+                .then(alert)
+        return;
+      }
+
+      this.$router.push({name: 'ListUsers'});
+    },
+    async fetchUser() {
+      const response = await ApiClient.get(`/users/${this.userId}`);
+
+      if (response.status === 404) {
+        this.$router.push({name: 'NotFoundError', message: `User #${this.userId} not found!`});
+        return null;
+      } else if (response.status !== 200) {
+        alert(`Unexpected error occurred: ${response.status}`);
+        return null;
+      } else {
+        return response.json();
+      }
+    }
+  }
+}
+</script>
