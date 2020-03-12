2b3e943d3e4c5deef84d5854273e0eff9715a447
Walter Kaunda <walterkaunda@live.co.uk>
2020-03-12 20:57:54 +0200

    Added font awesome and vuelidate.

--- src/main.js
+++ src/main.js
@@ -5,7 +5,9 @@ import store from './store'
 import moment from "moment";
 import ApiClient from "./services/api_client";
 import 'bootstrap'
-import 'bootstrap/dist/css/bootstrap.min.css'
+import 'bootstrap/dist/css/bootstrap.min.css';
+import '@fortawesome/fontawesome-free/css/all.css';
+
 Vue.config.productionTip = false
 Vue.prototype.moment = moment;
 new Vue({
