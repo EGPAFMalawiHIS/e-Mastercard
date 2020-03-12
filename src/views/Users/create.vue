37b30ef133f9e1bfd0f0cc125056f444733b8d74
Walter Kaunda <walterkaunda@live.co.uk>
2020-03-12 21:03:20 +0200

    Refactored User create page to use UserEditForm.

--- src/views/Users/create.vue
+++ src/views/Users/create.vue
@@ -1,14 +1,27 @@
 <template>
   <PageView>
-    <UserCreateForm />
+    <UserEditForm @on-submit="createUser" />
   </PageView>
 </template>
 
 <script>
 import PageView from "@/components/PageView";
-import UserCreateForm from "@/components/users/UserCreateForm"
+import UserEditForm from "@/components/users/UserEditForm"
 
 export default {
-  components: {PageView, UserCreateForm}
+  components: {PageView, UserEditForm},
+  methods: {
+    async createUser(user) {
+      const response = await ApiClient.post('/users', user); 
+
+      if (response.status == 400) {
+        response.text()
+                .then(alert)
+        return;
+      }
+
+      this.$router.push({name: 'ListUsers'});
+    }
+  }
 }
 </script>
