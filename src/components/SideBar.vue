2bd7e00a12719f330816f88cbe3cf03b43726dcb
Martin Suleman <sulemartin87@gmail.com>
2020-03-12 16:11:55 +0200

    worked on data cleaning tools

--- src/components/SideBar.vue
+++ src/components/SideBar.vue
@@ -5,7 +5,7 @@
         <a href="/" class="side-list list-group-item list-group-item-action ">Dashboard</a>
         <a href="/search" class="side-list list-group-item list-group-item-action ">Search</a>
         <a href="/reports" class="side-list list-group-item list-group-item-action ">Reports</a>
-        <a href="#" class="side-list list-group-item list-group-item-action ">Data cleaning tools</a>
+        <a href="/cleaning_tools" class="side-list list-group-item list-group-item-action ">Data cleaning tools</a>
         <!-- <a href="#" class="side-list list-group-item list-group-item-action ">Overview</a>
         <a href="#" class="side-list list-group-item list-group-item-action ">Events</a>
         <a href="#" class="side-list list-group-item list-group-item-action ">Profile</a>
@@ -45,11 +45,15 @@ export default {};
 #sidebar-wrapper .list-group {
   width: 15rem;
 }
+@media print {
+  .side-bar {
+    display: none;
+  }
+} 
 @media (min-width: 768px) {
   #sidebar-wrapper {
     margin-left: 0;
   }
-
   #page-content-wrapper {
     min-width: 0;
     width: 100%;
---
+++ src/views/DataCleaningTools.vue
@@ -0,0 +1,132 @@
+<template>
+  <div>
+    <div class="d-flex" id="wrapper">
+      <!-- Sidebar -->
+      <side-bar />
+      <!-- /#sidebar-wrapper -->
+
+      <!-- Page Content -->
+      <div id="page-content-wrapper">
+        <top-nav />
+        <!-- Page Content -->
+
+        <div class="container" id="btn-container">
+          <div class="row">
+            <div class="col" v-for="(tool, index) in tools" :key="index">
+              <div class="btn-container" @click="redirect(tool.path)">
+                <div class="card">
+                  <img src="../assets/mw.png" class="btn-icon" />
+                  <p>{{tool.name}}</p>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+        <!-- Page Content end -->
+      </div>
+    </div>
+  </div>
+</template>
+
+<style scope>
+#btn-container {
+  margin-top: 10px;
+}
+
+.card {
+  /* Add shadows to create the "card" effect */
+  box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.2);
+  transition: 0.3s;
+  text-align: center;
+  cursor: pointer;
+}
+
+/* On mouse-over, add a deeper shadow */
+.card:hover {
+  box-shadow: 0 8px 16px 0 rgba(0, 0, 0, 0.2);
+}
+
+/* Add some padding inside the card container */
+.btn-container {
+  padding: 2px 16px;
+  width: 230px;
+}
+
+.btn-icon {
+  width: 100px;
+  height: 100px;
+  padding-top: 12px;
+  display: block;
+  margin-left: auto;
+  margin-right: auto;
+}
+
+.card p {
+  font-weight: bold;
+}
+</style>
+
+
+
+
+<script>
+// @ is an alias to /src
+import TopNav from "@/components/topNav.vue";
+import Sidebar from "@/components/SideBar.vue";
+
+export default {
+  name: "reports",
+  components: {
+    "top-nav": TopNav,
+    "side-bar": Sidebar
+  },
+  methods: {
+    redirect: function(url) {
+      this.$router.push(url);
+    }
+  },
+  data: function() {
+    return {
+      tools: [
+        {
+          path:
+            "/cleaning_tools/date_enrolled_greater_than_earliest_start_date",
+          name: "Date enrolled greater than start date"
+        },
+        {
+          path: "/cleaning_tools/encounters_after_death",
+          name: "Encounters after death"
+        },
+        {
+          path: "/cleaning_tools/enrolled_before_birth",
+          name: "Enrolled before birth"
+        },
+        {
+          path: "/cleaning_tools/incomplete_visits",
+          name: "Incomplete Visits"
+        },
+        {
+          path: "/cleaning_tools/male_with_female_obs",
+          name: "Male with Female Obs"
+        },
+        {
+          path: "/cleaning_tools/missing_start_reason",
+          name: "missing start reason"
+        },
+        {
+          path: "/cleaning_tools/multiple_start_reason",
+          name: "Multiple start reasons"
+        },
+        {
+          path: "/cleaning_tools/pre_art_and_unkown_outocme",
+          name: "pre ART and unknown outcome"
+        },
+        {
+          path: "/cleaning_tools/prescription_without_dispensation",
+          name: "Prescription without dispensation"
+        }
+      ]
+    };
+  }
+};
+</script>
