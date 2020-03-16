f53da84d1755e35fb9237e6c882fed03e18859f6
Martin Suleman <sulemartin87@gmail.com>
2020-03-16 16:15:15 +0200

    added viral load

--- src/views/Mastercard.vue
+++ src/views/Mastercard.vue
@@ -29,7 +29,7 @@
             type="button"
             class="btn btn-primary"
             data-toggle="modal"
-            data-target="#exampleModalCenter"
+            data-target="#viral-load"
           >Viral Load</button>
           <div class="row" style="margin: 10px; margin-top: 5px;">
             <visit-data></visit-data>
@@ -58,7 +58,7 @@
             </div>
             
           </div>
- <div
+          <div
             class="modal fade"
             id="outcomeModal"
             tabindex="-1"
@@ -81,6 +81,29 @@
             </div>
             
           </div>
+          <div
+            class="modal fade"
+            id="viral-load"
+            tabindex="-1"
+            role="dialog"
+            aria-labelledby="viral-load"
+            aria-hidden="true"
+          >
+            <div class="modal-dialog modal-dialog-centered modal-xl" role="document">
+              <div class="modal-content">
+                <div class="modal-header">
+                  <h5 class="modal-title" id="exampleModalLongTitle">New Visit</h5>
+                  <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                    <span aria-hidden="true">&times;</span>
+                  </button>
+                </div>
+                <div class="modal-body">
+                  <viral-load  ref="viral-load"></viral-load>      
+                </div>
+              </div>
+            </div>
+            
+          </div>
         </div>
       </div>
       <!-- /#page-content-wrapper -->
@@ -97,6 +120,7 @@ import VisitData from "@/components/VisitData.vue";
 import clinicRegistration from "@/components/encounters/clinicRegistration.vue";
 import encounter from "@/components/encounters/encounter.vue"
 import outcome from "@/components/outcome.vue"
+import viralLoad from '@/components/encounters/viral_load.vue';
 export default {
   name: "home",
   components: {
@@ -106,7 +130,8 @@ export default {
     "visit-data": VisitData,
     "clinic-registration": clinicRegistration,
     "encounter": encounter,
-    "outcome": outcome
+    "outcome": outcome,
+    "viral-load": viralLoad
   }
 };
 </script>
