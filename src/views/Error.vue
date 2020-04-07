3d39e0f06ede9658de1e2a7066c6bd421e6732a2
Walter Kaunda <walterkaunda@live.co.uk>
2020-04-07 09:58:05 +0200

    Added an error page and refactored router.

---
+++ src/views/Error.vue
@@ -0,0 +1,26 @@
+<template>
+  <PageView>
+    <h1>Error</h1>
+    <p>{{ message() }}</p>
+  </PageView>
+</template>
+
+<script>
+import PageView from "@/components/PageView";
+
+export default {
+  name: "Users",
+  components: {PageView},
+  data() {
+    return {
+      users: [],
+      loading: true
+    }
+  },
+  methods: {
+    message() {
+      return this.$route.params['message'];
+    },
+  }
+}
+</script>
