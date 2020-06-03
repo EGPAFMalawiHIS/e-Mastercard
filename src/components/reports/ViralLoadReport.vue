4b40f97388bb78e9220950f7de31bd4064593af1
Jmwantisi <jamesmwantisi@gmail.com>
2020-06-03 04:46:11 +0200

    added initial retrieval of data for viral load

--- src/components/reports/ViralLoadReport.vue
+++ src/components/reports/ViralLoadReport.vue
@@ -1,52 +1,47 @@
 <template>
-      <!-- Page Content -->
-      <div id="main-container">
-        <div class="row">
-          <div class="col-sm-12" style="z-index: 30">
-            <span>
-              {{reportTitle}}
-              <button @click="$router.go(-1)" class="btn btn-primary">Back</button>
-            </span>
-            <sdPicker :onSubmit="fetchDates"></sdPicker>
-          </div>
-        </div>
-        <div class="row">
-          <div class="col-sm-12">
-            <ReportOverlay :reportLoading="reportLoading" :reportSelected="reportSelected">
-              <table class="table table-striped report" id="cohort-clients">
-                <thead>
-                  <tr>
-                    <th>#</th>
-                    <th>Age group</th>
-                    <th class="disaggregated-numbers">0P</th>
-                    <th class="disaggregated-numbers">2P</th>
-                    <th class="disaggregated-numbers">4P</th>
-                    <th class="disaggregated-numbers">9P</th>
-                    <th class="disaggregated-numbers">11P</th>
-                    <th class="disaggregated-numbers">0A</th>
-                    <th class="disaggregated-numbers">2A</th>
-                    <th class="disaggregated-numbers">4A</th>
-                    <th class="disaggregated-numbers">5A</th>
-                    <th class="disaggregated-numbers">6A</th>
-                    <th class="disaggregated-numbers">7A</th>
-                    <th class="disaggregated-numbers">8A</th>
-                    <th class="disaggregated-numbers">9A</th>
-                    <th class="disaggregated-numbers">10A</th>
-                    <th class="disaggregated-numbers">11A</th>
-                    <th class="disaggregated-numbers">12A</th>
-                    <th class="disaggregated-numbers">13A</th>
-                    <th class="disaggregated-numbers">14A</th>
-                    <th class="disaggregated-numbers">15A</th>
-                    <th class="disaggregated-numbers">16A</th>
-                    <th class="disaggregated-numbers">17A</th>
-                  </tr>
-                </thead>
-                <tbody ref="tableBody"></tbody>
-              </table>
-            </ReportOverlay>
-          </div>
-        </div>
-      </div>
+  <!-- Page Content -->
+  <div id="main-container" class="col-12 table-col">
+    <span>
+      {{report_title}}
+      <button @click="$router.go(-1)" class="btn btn-primary">Back</button>
+    </span>
+    <sdPicker :onSubmit="fetchDates"></sdPicker>
+    <table class="table table-striped report" id="cohort-clients">
+      <thead>
+        <tr>
+          <th>#</th>
+          <th>Age group</th>
+          <th class="disaggregated-numbers">0A</th>
+          <th class="disaggregated-numbers">2A</th>
+          <th class="disaggregated-numbers">4A</th>
+          <th class="disaggregated-numbers">5A</th>
+          <th class="disaggregated-numbers">6A</th>
+          <th class="disaggregated-numbers">7A</th>
+          <th class="disaggregated-numbers">8A</th>
+          <th class="disaggregated-numbers">9A</th>
+          <th class="disaggregated-numbers">10A</th>
+          <th class="disaggregated-numbers">11A</th>
+          <th class="disaggregated-numbers">12A</th>
+          <th class="disaggregated-numbers">13A</th>
+          <th class="disaggregated-numbers">14A</th>
+          <th class="disaggregated-numbers">15A</th>
+          <th class="disaggregated-numbers">16A</th>
+          <th class="disaggregated-numbers">17A</th>
+          <th class="disaggregated-numbers">0P</th>
+          <th class="disaggregated-numbers">2P</th>
+          <th class="disaggregated-numbers">4P</th>
+          <th class="disaggregated-numbers">9P</th>
+          <th class="disaggregated-numbers">11P</th>
+          <th class="disaggregated-numbers">14P</th>
+          <th class="disaggregated-numbers">15P</th>
+          <th class="disaggregated-numbers">16P</th>
+          <th class="disaggregated-numbers">17P</th>
+          <th class="disaggregated-numbers">Unknown</th>
+        </tr>
+      </thead>
+      <tbody ref="tableBody"></tbody>
+    </table>
+  </div>
 </template>
 
 <script>
@@ -56,14 +51,13 @@ require("@/assets/datatable/css/dataTables.bootstrap4.min.css");
 //require("@/assets/datatable/jquery-ui.css");
 require("@/assets/datatable/css/buttons.dataTables.min.css");
 require("@/assets/datatable/css/dataTables.jqueryui.min.css");
+require("@/assets/datatable/css/fixedColumns.dataTables.min.css");
 
 import ApiClient from "../../services/api_client";
 import TopNav from "@/components/topNav.vue";
 import Sidebar from "@/components/SideBar.vue";
-import { mapState } from "vuex";
 import moment from "moment";
 import StartAndEndDatePicker from "@/components/StartAndEndDatePicker.vue";
-import ReportOverlay from "./ReportOverlay";
 
 import jQuery from "jquery";
 import datatable from "datatables";
@@ -74,41 +68,71 @@ require("@/assets/datatable/js/pdfmake.min.js");
 require("@/assets/datatable/js/vfs_fonts.js");
 require("@/assets/datatable/js/buttons.html5.min.js");
 require("@/assets/datatable/js/buttons.print.min.js");
+require("@/assets/datatable/js/dataTables.fixedHeader.min.js");
 
 export default {
   name: "reports",
   components: {
-    ReportOverlay,
     "top-nav": TopNav,
     "side-bar": Sidebar,
     sdPicker: StartAndEndDatePicker
   },
+  data: function() {
+    return {
+      report_title: "Viral Load Report ",
+      reportData: null,
+      dTable: null,
+      formatedData: [],
+      rebuildOutcome: true,
+      startDate: null,
+      endDate: null,
+      AGE_GROUPS: [
+        "0 - 1 Years",
+        "2 - 4 Years",
+        "5 - 9 Years",
+        "10 - 14 Years",
+        "15 - 17 Years",
+        "18 - 19 Years",
+        "20 - 24 Years",
+        "25 - 29 Years",
+        "30 - 34 Years",
+        "35 - 39 Years",
+        "40 - 44 Years",
+        "45 - 50 Years",
+        "50 Years +"
+      ].reverse(),
+      viralLoadReportData: {},
+      viralLoadReport: {}
+    };
+  },
   methods: {
-    async fetchDates(dates) {
-      try {
-        [this.startDate, this.endDate] = dates;
-
-        const isValidDate = date => date && date !== "Invalid date";
-
-        if (!(isValidDate(this.startDate) && isValidDate(this.endDate))) return;
-
-        this.reportLoading = true;
-        this.reportSelected = true;
-        await this.initializeReport();
-      } catch (e) {
-        this.$router.push({ name: "error", params: { message: e.message } });
-      } finally {
-        this.reportLoading = false;
-      }
+    fetchDates: function(dates) {
+      this.fetchReport(dates);
     },
     initDataTable() {
+      let start_date = moment(this.startDate).format("DD/MMM/YYYY");
+      let end_date = moment(this.endDate).format("DD/MMM/YYYY");
+      this.report_title = sessionStorage.location_name + " Viral Load Report. ";
+
+      if (!start_date == "Invalid date") {
+        this.report_title += " Reporting  period: " + start_date;
+        this.report_title += " " + end_date;
+      }
+
       this.dTable = jQuery("#cohort-clients").dataTable({
         order: [[0, "asc"]],
-        fixedHeader: true,
+        fixedHeader: false,
         searching: false,
         paging: false,
-        Processing: true,
-        ServerSide: true,
+        Processing: false,
+        ServerSide: false,
+        scrollY: "50vh",
+        scrollX: true,
+        scrollCollapse: true,
+        fixedColumns: {
+          leftColumns: 2,
+          rightColumns: 1
+        },
         scroller: {
           loadingIndicator: true
         },
@@ -116,91 +140,94 @@ export default {
         buttons: [
           {
             extend: "copy",
-            title: this.reportTitle
+            title: this.report_title
           },
           {
             extend: "csv",
-            title: this.reportTitle
+            title: this.report_title
           },
           {
             extend: "pdf",
-            title: this.reportTitle
+            title: this.report_title
           },
           {
             extend: "print",
-            title: this.reportTitle
+            title: this.report_title
           }
         ]
       });
     },
-    addTableBody() {
-      let columns = [
-        "0-5 months",
-        "6-11 months",
-        "12-23 months",
-        "2-4 years",
-        "5-9 years",
-        "10-14 years",
-        "15-17 years",
-        "18-19 years",
-        "20-24 years",
-        "25-29 years",
-        "30-34 years",
-        "35-39 years",
-        "40-44 years",
-        "45-49 years",
-        "50 plus years"
-      ];
+    fetchReport: async function(dates = {}) {
+      const startDate = moment(dates[0]).format("YYYY-MM-DD");
+      const endDate = moment(dates[1]).format("YYYY-MM-DD");
 
-      setTimeout(() => this.initDataTable(), 300);
-    },
-    initializeReport: async function() {
-      
-    },
-    async addData(data) {
+      const url = `programs/1/reports/vl_disaggregated?start_date=${startDate}&end_date=${endDate}&from=0&to=10000000`; // rememeber to Fix From and To
 
+      let btns = document.getElementsByClassName("dt-button");
+      for (let i = 0; i < btns.length; i++) {
+        btns[i].setAttribute("disabled", true);
+        btns[i].style = "display: none;";
+      }
+
+      const response = await ApiClient.get(url, {}, {});
+      if (response.status === 200) {
+        response.json().then(data => {
+          this.viralLoadReportData = data;
+          console.log(this.viralLoadReportData);
+          this.buildReportData();
+        }); // outputing report data
+      }
     },
-  },
-  data: function() {
-    return {
-      reportData: null,
-      dTable: null,
-      formatedData: [],
-      rebuildOutcome: true,
-      startDate: null,
-      endDate: null,
-      screenedTB: [],
-      givenIPT: [],
-      totalMales: [0, 0, 0, 0],
-      totalFemales: [0, 0, 0, 0],
-      fpRow: null,
-      fbfRow: null,
-      ageGroups: [
-        "0-5 months",
-        "6-11 months",
-        "12-23 months",
-        "2-4 years",
-        "5-9 years",
-        "10-14 years",
-        "15-17 years",
-        "18-19 years",
-        "20-24 years",
-        "25-29 years",
-        "30-34 years",
-        "35-39 years",
-        "40-44 years",
-        "45-49 years",
-        "50 plus years"
-      ].reverse(),
-      reportLoading: false,
-      reportSelected: false
-    };
-  },
-  computed: {
-    ...mapState(["location"]),
-    reportTitle() {
-      return `${this.location.name} Viral Load Report`;
+
+    buildReportData() {
+      this.viralLoadReport = this.AGE_GROUPS.map(ageGroup => {
+        return this.viralLoadData(ageGroup);
+      });
+      console.log(this.viralLoadReport)
+    },
+
+    viralLoadData(ageGroup = {}) {
+      return {
+        age_group: ageGroup,
+        zero_a: this.report(ageGroup, "0A") || 0,
+        two_a: this.report(ageGroup, "2A") || 0,
+        four_a: this.report(ageGroup, "4A") || 0,
+        five_a: this.report(ageGroup, "5A") || 0,
+        six_a: this.report(ageGroup, "6A") || 0,
+        seven_a: this.report(ageGroup, "7A") || 0,
+        eight_a: this.report(ageGroup, "8A") || 0,
+        nine_a: this.report(ageGroup, "9A") || 0,
+        ten_a: this.report(ageGroup, "10A") || 0,
+        eleven_a: this.report(ageGroup, "11A") || 0,
+        twelve_a: this.report(ageGroup, "12A") || 0,
+        thirteen_a: this.report(ageGroup, "13A") || 0,
+        fifteen_a: this.report(ageGroup, "15A") || 0,
+        sixteen_a: this.report(ageGroup, "16A") || 0,
+        seventeen_a: this.report(ageGroup, "17A") || 0,
+        zero_p: this.report(ageGroup, "0A") || 0,
+        two_p: this.report(ageGroup, "2P") || 0,
+        four_p: this.report(ageGroup, "4A") || 0,
+        nine_p: this.report(ageGroup, "9A") || 0,
+        eleven_p: this.report(ageGroup, "11P") || 0,
+        fourteen_p: this.report(ageGroup, "14P") || 0,
+        fifteen_p: this.report(ageGroup, "15P") || 0,
+        sixteen_p: this.report(ageGroup, "16P") || 0,
+        seventeen_p: this.report(ageGroup, "17P") || 0
+      };
+    },
+
+    // return the count
+    report(ageGroup, regimen) {
+
+      if(this.viralLoadReportData[ageGroup] != undefined){
+        return this.viralLoadReportData[ageGroup][regimen]
+      }else{
+        return 0
+      }
     }
+  },
+  mounted() {
+    setTimeout(() => this.initDataTable(), 300);
   }
 };
 </script>
@@ -237,5 +264,15 @@ table {
 
 .show-btn {
   font-size: 14px;
+}
+
+/* Ensure that the demo table scrolls */
+th,
+td {
+  white-space: nowrap;
+}
+div.dataTables_wrapper {
+  /* width: 800px;*/
+  margin: 0 auto;
 }
 </style>
