337fd1caafb187b34ddc62b95981caf86704d31e
Jmwantisi <jamesmwantisi@gmail.com>
2020-06-03 16:10:21 +0200

    added Overlay to report

--- src/components/reports/ViralLoadReport.vue
+++ src/components/reports/ViralLoadReport.vue
@@ -34,82 +34,85 @@
           </div>
         </div>
       </div>
-          <div class="col-md-2">
-
-          </div>
-      <div class="col-md-5">
+      <div class="col-md-2"></div>
+      <div class="col-md-5" style="z-index: 30">
         <div class="form-group">
           <sdPicker :onSubmit="fetchDates"></sdPicker>
         </div>
       </div>
     </div>
-
-    <table class="table table-striped report" id="cohort-clients">
-      <thead>
-        <tr>
-          <th>#</th>
-          <th>Age group</th>
-          <th class="disaggregated-numbers">0A</th>
-          <th class="disaggregated-numbers">2A</th>
-          <th class="disaggregated-numbers">4A</th>
-          <th class="disaggregated-numbers">5A</th>
-          <th class="disaggregated-numbers">6A</th>
-          <th class="disaggregated-numbers">7A</th>
-          <th class="disaggregated-numbers">8A</th>
-          <th class="disaggregated-numbers">9A</th>
-          <th class="disaggregated-numbers">10A</th>
-          <th class="disaggregated-numbers">11A</th>
-          <th class="disaggregated-numbers">12A</th>
-          <th class="disaggregated-numbers">13A</th>
-          <th class="disaggregated-numbers">14A</th>
-          <th class="disaggregated-numbers">15A</th>
-          <th class="disaggregated-numbers">16A</th>
-          <th class="disaggregated-numbers">17A</th>
-          <th class="disaggregated-numbers">0P</th>
-          <th class="disaggregated-numbers">2P</th>
-          <th class="disaggregated-numbers">4P</th>
-          <th class="disaggregated-numbers">9P</th>
-          <th class="disaggregated-numbers">11P</th>
-          <th class="disaggregated-numbers">14P</th>
-          <th class="disaggregated-numbers">15P</th>
-          <th class="disaggregated-numbers">16P</th>
-          <th class="disaggregated-numbers">17P</th>
-          <th class="disaggregated-numbers">Unknown</th>
-        </tr>
-      </thead>
-      <tbody>
-        <tr v-for="(item, index) in viralLoadReport" :key="index">
-          <th scope="row">{{index + 1}}</th>
-          <td>{{item.age_group}}</td>
-          <td>{{item.zero_a}}</td>
-          <td>{{item.two_a}}</td>
-          <td>{{item.four_a}}</td>
-          <td>{{item.five_a}}</td>
-          <td>{{item.six_a}}</td>
-          <td>{{item.seven_a}}</td>
-          <td>{{item.eight_a}}</td>
-          <td>{{item.nine_a}}</td>
-          <td>{{item.ten_a}}</td>
-          <td>{{item.eleven_a}}</td>
-          <td>{{item.twelve_a}}</td>
-          <td>{{item.thirteen_a}}</td>
-          <td>{{item.fourteen_a}}</td>
-          <td>{{item.fifteen_a}}</td>
-          <td>{{item.sixteen_a}}</td>
-          <td>{{item.seventeen_a}}</td>
-          <td>{{item.zero_p}}</td>
-          <td>{{item.two_p}}</td>
-          <td>{{item.four_p}}</td>
-          <td>{{item.nine_p}}</td>
-          <td>{{item.eleven_p}}</td>
-          <td>{{item.fourteen_p}}</td>
-          <td>{{item.fifteen_p}}</td>
-          <td>{{item.sixteen_p}}</td>
-          <td>{{item.seventeen_p}}</td>
-          <td>{{item.unknown}}</td>
-        </tr>
-      </tbody>
-    </table>
+    <div class="row">
+      <div class="col-md-12">
+        <ReportOverlay :reportLoading="reportLoading" :reportSelected="reportSelected">
+          <table class="table table-striped report" id="cohort-clients">
+            <thead>
+              <tr>
+                <th>#</th>
+                <th>Age group</th>
+                <th class="disaggregated-numbers">0A</th>
+                <th class="disaggregated-numbers">2A</th>
+                <th class="disaggregated-numbers">4A</th>
+                <th class="disaggregated-numbers">5A</th>
+                <th class="disaggregated-numbers">6A</th>
+                <th class="disaggregated-numbers">7A</th>
+                <th class="disaggregated-numbers">8A</th>
+                <th class="disaggregated-numbers">9A</th>
+                <th class="disaggregated-numbers">10A</th>
+                <th class="disaggregated-numbers">11A</th>
+                <th class="disaggregated-numbers">12A</th>
+                <th class="disaggregated-numbers">13A</th>
+                <th class="disaggregated-numbers">14A</th>
+                <th class="disaggregated-numbers">15A</th>
+                <th class="disaggregated-numbers">16A</th>
+                <th class="disaggregated-numbers">17A</th>
+                <th class="disaggregated-numbers">0P</th>
+                <th class="disaggregated-numbers">2P</th>
+                <th class="disaggregated-numbers">4P</th>
+                <th class="disaggregated-numbers">9P</th>
+                <th class="disaggregated-numbers">11P</th>
+                <th class="disaggregated-numbers">14P</th>
+                <th class="disaggregated-numbers">15P</th>
+                <th class="disaggregated-numbers">16P</th>
+                <th class="disaggregated-numbers">17P</th>
+                <th class="disaggregated-numbers">Unknown</th>
+              </tr>
+            </thead>
+            <tbody>
+              <tr v-for="(item, index) in viralLoadReport" :key="index">
+                <th scope="row">{{index + 1}}</th>
+                <td>{{item.age_group}}</td>
+                <td>{{item.zero_a}}</td>
+                <td>{{item.two_a}}</td>
+                <td>{{item.four_a}}</td>
+                <td>{{item.five_a}}</td>
+                <td>{{item.six_a}}</td>
+                <td>{{item.seven_a}}</td>
+                <td>{{item.eight_a}}</td>
+                <td>{{item.nine_a}}</td>
+                <td>{{item.ten_a}}</td>
+                <td>{{item.eleven_a}}</td>
+                <td>{{item.twelve_a}}</td>
+                <td>{{item.thirteen_a}}</td>
+                <td>{{item.fourteen_a}}</td>
+                <td>{{item.fifteen_a}}</td>
+                <td>{{item.sixteen_a}}</td>
+                <td>{{item.seventeen_a}}</td>
+                <td>{{item.zero_p}}</td>
+                <td>{{item.two_p}}</td>
+                <td>{{item.four_p}}</td>
+                <td>{{item.nine_p}}</td>
+                <td>{{item.eleven_p}}</td>
+                <td>{{item.fourteen_p}}</td>
+                <td>{{item.fifteen_p}}</td>
+                <td>{{item.sixteen_p}}</td>
+                <td>{{item.seventeen_p}}</td>
+                <td>{{item.unknown}}</td>
+              </tr>
+            </tbody>
+          </table>
+        </ReportOverlay>
+      </div>
+    </div>
   </div>
 </template>
 
@@ -130,6 +133,7 @@ import StartAndEndDatePicker from "@/components/StartAndEndDatePicker.vue";
 
 import jQuery from "jquery";
 import datatable from "datatables";
+import ReportOverlay from "./ReportOverlay";
 
 require("@/assets/datatable/js/buttons.flash.min.js");
 require("@/assets/datatable/js/jszip.min.js");
@@ -144,7 +148,8 @@ export default {
   components: {
     "top-nav": TopNav,
     "side-bar": Sidebar,
-    sdPicker: StartAndEndDatePicker
+    sdPicker: StartAndEndDatePicker,
+    ReportOverlay
   },
   data: function() {
     return {
@@ -173,12 +178,32 @@ export default {
         "50 Years +"
       ],
       viralLoadReportData: {},
-      viralLoadReport: {}
+      viralLoadReport: {},
+      reportLoading: false,
+      reportSelected: false
     };
   },
   methods: {
-    fetchDates: function(dates) {
-      this.fetchReport(dates);
+    // fetchDates: function(dates) {
+    //   this.fetchReport(dates);
+    // },
+    async fetchDates(dates) {
+      try {
+        [this.startDate, this.endDate] = dates;
+
+        const isValidDate = date => date && date !== "Invalid date";
+
+        if (!(isValidDate(this.startDate) && isValidDate(this.endDate))) return;
+
+        this.reportLoading = true;
+        this.reportSelected = true;
+        await this.fetchReport(dates);
+        //await this.initializeReport();
+      } catch (e) {
+        this.$router.push({ name: "error", params: { message: e.message } });
+      } finally {
+        this.reportLoading = false;
+      }
     },
     initDataTable() {
       let start_date = moment(this.startDate).format("DD/MMM/YYYY");
@@ -233,7 +258,7 @@ export default {
       const endDate = moment(dates[1]).format("YYYY-MM-DD");
 
       const url = `programs/1/reports/vl_disaggregated?start_date=${startDate}&end_date=${endDate}&from=${this.minViralLoad}&to=${this.maxViralLoad}`; // rememeber to Fix From and To
-      
+
       let btns = document.getElementsByClassName("dt-button");
       for (let i = 0; i < btns.length; i++) {
         btns[i].setAttribute("disabled", true);
