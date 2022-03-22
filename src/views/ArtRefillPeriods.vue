4726869f844d3e4175f1bc43524d687c1aeeb22f
Andrew Mfune <andiemfune@gmail.com>
2022-03-22 16:10:41 +0200

    Seperating quarter dates with To

--- src/views/ArtRefillPeriods.vue
+++ src/views/ArtRefillPeriods.vue
@@ -265,7 +265,7 @@ export default {
 
       y += "\n";
       y += `Date Created:  ${moment().format("YYYY-MM-DD:h:m:s")} 
-                          Quarter: ${this.startDate}-${this.endDate}
+                          Quarter: ${this.startDate} to ${this.endDate}
                           e-Mastercard Version : ${sessionStorage.EMCVersion} 
                           API Version ${sessionStorage.APIVersion}`;
       for (let index = 0; index < 34; index++) {
--- src/views/DefaulterList.vue
+++ src/views/DefaulterList.vue
@@ -177,7 +177,7 @@ export default {
 
       y += "\n";
       y += `Date Created:  ${moment().format("YYYY-MM-DD:h:m:s")} 
-                          Quarter: ${this.startDate}-${this.endDate}
+                          Quarter: ${this.startDate} to ${this.endDate}
                           e-Mastercard Version : ${sessionStorage.EMCVersion}
                           Site UUID: ${sessionStorage.siteUUID} 
                           API Version ${sessionStorage.APIVersion}`;
