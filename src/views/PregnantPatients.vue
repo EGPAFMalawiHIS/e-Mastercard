ced46df90695fffd7784228a98a2d68fcf4ad208
Daniel Justin <danieljmuleke@gmail.com>
2022-07-07 21:37:06 +0200

    Hide client names on exported pregnant patient report

--- src/views/PregnantPatients.vue
+++ src/views/PregnantPatients.vue
@@ -2,172 +2,146 @@
     <div class="d-flex" id="wrapper">
       <side-bar />
       <div id="page-content-wrapper">
-         <top-nav />
+        <top-nav />
         <!-- Page Content -->
         <div id="main-container">
           <div class="row">
             <div class="col-sm-12" style="z-index: 20"> <!-- elevate date picker above overlay below -->
-             <span>{{report_title}}<button @click="$router.go(-1)" class="btn btn-primary">Back</button></span>  
-            <sdPicker :onSubmit="fetchDates"></sdPicker>
-
+              <sdPicker :onSubmit="fetchDates"></sdPicker>
             </div>
           </div>
-         <div class="row">
-             <div class="col-sm-12">
-   <b-overlay :show="hideReport" spinner-type="grow" spinner-variant="primary">
-                  <template v-if="!reportSelected" v-slot:overlay>
-                    <h1>No Report Selected</h1>
-                  </template>
-          <table class="table table-striped report" id="cohort-clients">
-            <thead>
-              <tr>
-                <th scope="col">ARV number</th>
-                <th scope="col">First name</th>
-                <th scope="col">Last name</th>
-                <th class="center-text" scope="col">Gender</th>
-                <th class="center-text" scope="col">DOB</th>
-                <th class="center-text" scope="col">Action</th>
-              </tr>
-            </thead>
-            <tbody>
-              <tr v-for="(item, index) in reportData" :key="index">
-                <td>{{item.arv_number}}</td>
-                <td>{{item.given_name}}</td>
-                <td>{{item.family_name}}</td>
-                <td>{{item.gender}}</td>
-                <td>{{item.birthdate}}</td>
-                <td><button @click="$router.push(`/patient/mastercard/${item.patient_id}`)" class="btn-warning show-btn">Show</button></td>
-              </tr>
-            </tbody>
-              <tfoot>
-              <tr>
-                <td>
-                  Date Created:  {{moment().format('YYYY-MM-DD:h:m:s')}} 
-                  e-Mastercard Version : {{EMCVersion}} 
-                  API Version {{APIVersion}}
-                </td>
-              </tr>
-            </tfoot>
-          </table>
-   </b-overlay>
-
-        </div>
-        </div>
-        </div>
-        <!-- Page Content end -->
+          <vue-bootstrap4-table
+            :rows="rows"
+            :columns="columns"
+            :config="config"
+            :show-loader="showLoader"
+            :actions="actions"
+            @on-download="onDownload"
+          >
+            <template slot="sort-asc-icon">&#8593;</template>
+            <template slot="sort-desc-icon">&#8595;</template>
+            <template slot="no-sort-icon">&#8593;&#8595;</template>
+            <template slot="birthdate" slot-scope="props">
+              {{ moment(props.cell_value).format("DD/MMM/YYYY") }}
+            </template>
+            <template slot="patient_id" slot-scope="props">
+              <button
+                type="button"
+                class="btn btn-primary"
+                @click="redirect(props.cell_value)"
+              >
+                show
+              </button>
+            </template>
+          </vue-bootstrap4-table>
+      </div>
+      <!-- Page Content end -->
     </div>
   </div>
 </template>
 
-<script>
-
-require("@/assets/datatable/css/bootstrap.css");
-require("@/assets/datatable/css/dataTables.bootstrap4.min.css");
-
-//require("@/assets/datatable/jquery-ui.css");
-require("@/assets/datatable/css/buttons.dataTables.min.css");
-require("@/assets/datatable/css/dataTables.jqueryui.min.css");
-
-import ApiClient from "../services/api_client";
+<script>import ApiClient from "../services/api_client";
 import TopNav from "@/components/topNav.vue";
 import Sidebar from "@/components/SideBar.vue";
 import moment from 'moment';
 import StartAndEndDatePicker from "@/components/StartAndEndDatePicker.vue";
-import jQuery from 'jquery';
-import datatable from 'datatables';
-
-
-
-require("@/assets/datatable/js/buttons.flash.min.js");
-require("@/assets/datatable/js/jszip.min.js");
-require("@/assets/datatable/js/pdfmake.min.js");
-require("@/assets/datatable/js/vfs_fonts.js");
-require("@/assets/datatable/js/buttons.html5.min.js");
-require("@/assets/datatable/js/buttons.print.min.js");
-
-
-
-
+import VueBootstrap4Table from "vue-bootstrap4-table";
+import { exportToCSV } from "../utils/exports";
 
 export default {
   name: "reports",
   components: {
     "top-nav": TopNav,
     "side-bar": Sidebar,
-    "sdPicker": StartAndEndDatePicker
-  },methods: {
-    fetchDates: async function(date) {
-      this.reportLoading = true;
-      this.reportSelected = true;
-      this.report_title = 'Clinic ' + sessionStorage.location_name + " patients pregnant report ";
-      this.report_title += moment(date[0]).format('DDMMMYYYY');
-      this.report_title += " - " + moment(date[1]).format('DDMMMYYYY');
-      let url_path = '/programs/1/reports/pregnant_patients?start_date=' + date[0] + 'end_date='+date[1];
+    "sdPicker": StartAndEndDatePicker,
+    "vue-bootstrap4-table": VueBootstrap4Table
+  },
+  data: function() {
+    return {
+      startDate: '',
+      endDate: '',
+      showLoader: false,
+      rows: [],
+      columns: [
+        {
+          label: "ARV Number",
+          name: "arv_number",
+          sort: true,
+          initial_sort: true,
+          initial_sort_order: "asc",
+        },
+        {
+          label: "First Name",
+          name: "given_name",
+          exportable: false,
+          sort: true,
+        },
+        {
+          label: "Last Name",
+          name: "family_name",
+          exportable: false,
+          sort: true,
+        },
+        {
+          label: "Gender",
+          name: "gender",
+          sort: true
+        },
+        {
+          label: "DOB",
+          name: "birthdate",
+          slot_name: "birthdate",
+          sort: true
+        },
+        {
+          label: "Actions",
+          name: "patient_id",
+          slot_name: "patient_id",
+          exportable: false,
+        },
+      ],
+      config: {
+        card_title: "Pregnant Patients Clinic Report ",
+        show_refresh_button: false,
+        show_reset_button: false,
+      },
+      actions: [
+        {
+          btn_text: "CSV",
+          event_name: "on-download",
+          class: "btn btn-primary my-custom-class",
+          event_payload: {
+            msg: "my custom msg",
+          },
+        },
+      ],
+    }
+  }, 
+  methods: {
+    async fetchDates(dates) {
+      this.showLoader = true;
+      this.startDate = dates[0]
+      this.endDate = dates[1]
+      this.config.card_title += moment(dates[0]).format('DD/MMM/YYYY') + " - " + moment(dates[1]).format('DDMMMYYYY');
+      let url_path = '/programs/1/reports/pregnant_patients?start_date=' + dates[0] + 'end_date='+dates[1];
       url_path += '&paginate=false';
       const response = await ApiClient.get(url_path, {}, {});
 
       if (response.status === 200) {
-        response.json().then((data) => this.checkResult(data) );
-      }else{
-        // setTimeout(() => this.fetchData(), 5000);
+        response.json().then((data) => this.rows = data);
       }
+      this.showLoader = false;
     },
-    initDataTable(){
-      this.dTable = jQuery("#cohort-clients").dataTable({
-        order: [[ 0, "asc" ]],
-        fixedHeader: true,
-        dom: 'Bfrtip',
-        retrieve: true,
-        buttons: [
-          {
-            extend: 'copy',
-            title:  this.report_title
-          },
-          {
-            extend: 'csv',
-            title:  this.report_title,
-            footer: true
-          },
-          {
-            extend: 'pdf',
-            title:  this.report_title
-          },
-          {
-            extend: 'print',
-            title:  this.report_title
-          }
-        ],
-        columnDefs: [
-          // {"className": "center-text", "targets": 3},
-          // {"className": "center-text", "targets": 4}
-        ]
+    redirect(id) {
+      this.$router.push(`/patient/mastercard/${id}`);
+    },
+    onDownload() {
+      exportToCSV(this.columns, this.rows, this.config.card_title, {
+        startDate: this.startDate,
+        endDate: this.endDate,
       });
     },
-    checkResult(data){
-      this.reportData = data;
-      this.reportLoading = false;
-      setTimeout(() => this.initDataTable(), 1000);
-    },
   },
-  mounted() {
-    // setTimeout(() => this.initDataTable(), 300);
-  }, data: function() {
-      return {
-        report_title: 'Pregnant Patients',
-        reportData: null,
-        dTable: null,
-        formatedData: [],
-        reportSelected: false,
-        reportLoading: false,
-        APIVersion: sessionStorage.APIVersion,
-        EMCVersion: sessionStorage.EMCVersion,
-      }
-    }, 
-    computed: {
-      hideReport() {
-        return this.reportLoading || !this.reportSelected;
-      },
-    }
 }
 
 </script>
