2e7d68c1895a05d63d1ed92b53752a9e3032fc39
Daniel Justin <danieljmuleke@gmail.com>
2022-11-24 15:15:51 +0200

    Added military site settings

--- src/views/Settings.vue
+++ src/views/Settings.vue
@@ -4,13 +4,17 @@
       <side-bar />
       <div id="page-content-wrapper">
         <top-nav />
-            <br>
-        <div class="container-fluid">
-            <site-name />            
-            <br>
-            <site-prefix />            
-            <br>
-            <site-uuid />
+        <div class="container-fluid mt-5">
+          <site-name class="mb-4" />       
+          <site-prefix class="mb-4" />  
+          <site-uuid class="mb-4" />
+          <general-preference label="Is military site?" :property="Properties.MILITARY_SITE" /> 
+          <!-- <div class="row">
+            <div class="col-10 offset-1 my-5">
+              <h3 class="text-left">General Preferences:</h3>  
+                         
+            </div>
+          </div> -->
         </div>
       </div>
     </div>
@@ -21,24 +25,22 @@
 import TopNav from "@/components/topNav.vue";
 import Sidebar from "@/components/SideBar.vue";
 import siteName from "@/components/settings/siteName.vue";
-import ApiClient from "../services/api_client";
-import "vue-select/dist/vue-select.css";
-import VueSelect from "vue-select";
 import sitePrefix from '../components/settings/sitePrefix.vue';
 import siteUUID from '../components/settings/siteUUID.vue';
+import GeneralPreference from '../components/settings/GeneralPreference.vue.vue';
+import { Properties } from "../services/global_properties";
+
 export default {
   components: {
     "top-nav": TopNav,
     "side-bar": Sidebar,
     "site-name": siteName,
     "site-prefix": sitePrefix,
-    "site-uuid": siteUUID
+    "site-uuid": siteUUID,
+    GeneralPreference,
   },
-    data: function() {
-        return {
-        }
-    }, methods: {
-        
-    }
+  data: () => ({
+    Properties
+  })
 };
 </script>
