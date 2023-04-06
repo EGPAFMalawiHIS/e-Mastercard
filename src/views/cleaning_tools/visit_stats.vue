b14d14d1a338d4d971d0777422c5aa82378bdb3d
Daniel Justin <danieljmuleke@gmail.com>
2023-04-06 11:53:26 +0200

    Patient Visit stats update (#93)
    * Refactor: removed unneccessary comments
    
    * Added statistics summary section
    
    * Implemented summary drill down
    
    * Show visit dates
    
    * Contextualize drill down titles

--- src/views/cleaning_tools/visit_stats.vue
+++ src/views/cleaning_tools/visit_stats.vue
@@ -4,44 +4,71 @@
       <div id="page-content-wrapper">
          <top-nav />
         <!-- Page Content -->
-        <div id="main-container" class="col-12 table-col">
-           <sdPicker :onSubmit="fetchData"></sdPicker>
-           <!--div id="chart-container" style="margin-top:50px; width: 100%; height: 90%;"-->
-           <highcharts :options="chartOptions" style="width: 100%;"></highcharts>
-           </div>
+        <div id="main-container">
+          <div class="row">
+            <div class="col-md-12 mb-4">
+              <sdPicker :onSubmit="fetchData"></sdPicker>
+            </div>
+            <div class="col-lg-3">
+              <b-card header="Statistics Summary" class="text-left mx-2">
+                <b-list-group class="m-0">
+                  <b-list-group-item class="d-flex justify-content-between align-items-center">
+                    <span>Total Attendance:</span>
+                    <b-badge pill variant="primary" href="#" @click="onSummaryDrill(totalAttendance, 'Total attendance')">{{ totalAttendance.length }}</b-badge>
+                  </b-list-group-item>
+                  <b-list-group-item class="d-flex justify-content-between align-items-center">
+                    <span>Patient visit:</span>
+                    <b-badge pill variant="primary" href="#" @click="onSummaryDrill(totalAttendance, 'Total patient visits')">{{ patientVisits.length }}</b-badge>
+                  </b-list-group-item>
+                  <b-list-group-item class="d-flex justify-content-between align-items-center">
+                    <span>Guardian visit:</span>
+                    <b-badge pill variant="primary" href="#" @click="onSummaryDrill(totalAttendance, 'Guardian visits')">{{ guardianVisits.length }}</b-badge>
+                  </b-list-group-item>
+                </b-list-group>
+              </b-card>
+            </div>
+            <div class="col-lg-9">
+              <b-card class="mr-2">
+                <highcharts :options="chartOptions" style="width: 100%;"></highcharts>
+              </b-card>
+            </div>
+          </div>
         </div>
-        <!-- Page Content end -->
+      </div>
+      <b-modal id="modal-1" :title="drillTitle" size="xl">
+        <!-- btable  -->
+        <b-table
+          striped
+          hover
+          id="my-table"
+          :items="drillClients"
+          :per-page="perPage"
+          :current-page="currentPage"
+        ></b-table>
+        <b-pagination
+          v-model="currentPage"
+          :per-page="perPage"
+          :total-rows="drillClients.length"
+          aria-controls="my-table"
+        ></b-pagination>
+
+        <p class="mt-3">Current Page: {{ currentPage }}</p>
+      </b-modal>
+      <!-- Page Content end -->
     </div>
-  </div>
 </template>
-
-<style scope>
-
-</style>
-
 <script type="text/javascript" src="../../../src/assets/HighCharts/highcharts.js"></script>
 <script type="text/javascript" src="../../../src/assets/HighCharts/highcharts-vue.min.js"></script>
-
 
 <script>
 // @ is an alias to /src
 import TopNav from "@/components/topNav.vue";
 import Sidebar from "@/components/SideBar.vue";
-
 import ApiClient from "../../services/api_client";
 import moment from 'moment';
 import StartAndEndDatePicker from "@/components/StartAndEndDatePicker.vue";
-import jQuery from 'jquery'
-
 import {Chart} from 'highcharts-vue'
-
-
-/*require("@/assets/HighCharts/code/highcharts.js");
-require("@/assets/HighCharts/code/modules/data.js");
-require("@/assets/HighCharts/code/modules/exporting.js");
-require("@/assets/HighCharts/code/modules/export-data.js");
-require("@/assets/HighCharts/code/modules/accessibility.js");*/
-
+import { formatGender } from '../../utils/str';
 
 export default {
   name: "cleaning_tools",
@@ -69,91 +96,96 @@ export default {
       }else{
       }
     },
-    checkResult(visits){
-      for(let visit_date in visits){
-        this.visit_dates.push(visit_date);
-      }
-      this.visit_dates = this.visit_dates.sort();
-      let visit_dates = this.visit_dates;
-
-      let patient_visit_type_hash = {};
-      let guardian_visit_type_hash = {};
-      let patient_guardian_visit_type_hash = {};
-
-      for(var i = 0; i < visit_dates.length; i++){
-        patient_visit_type_hash[visit_dates[i]] = 0;
-        guardian_visit_type_hash[visit_dates[i]] = 0;
-        patient_guardian_visit_type_hash[visit_dates[i]] = 0;
-
-        for(let visit_date in visits){
-          if(visit_date != visit_dates[i])
-            continue;
-
-          let data = visits[visit_date];
-
-          for(let patient_id in data) {
-
-            if(visits[visit_date][patient_id].guardian_present == true
-              && visits[visit_date][patient_id].patient_present == true) {
-                patient_guardian_visit_type_hash[visit_dates[i]] += 1;
-                continue;
-            }
-
-            if(visits[visit_date][patient_id].patient_present == true)
-              patient_visit_type_hash[visit_dates[i]] += 1;
-
-            if(visits[visit_date][patient_id].guardian_present == true)
-              guardian_visit_type_hash[visit_dates[i]] += 1;
-
-          }
-
-        }
-      }
-      this.sortReadyForRendering(patient_visit_type_hash, guardian_visit_type_hash,
-      patient_guardian_visit_type_hash);
-
-      this.chartOptions = {
-        series: [{
-          name: "Patient present",
-          data: this.sorted_data_patients
-        }, {
-          name: "Guardian present",
-          data: this.sorted_data_guardians
-        }, {
-          name: "Both patient and guardian present",
-          data: this.sorted_data_both_patient_guardians
-        }]
-      }
-
-
+    updateChart (seriesData) {
+      this.chartOptions.series = [
+        { name: "Patient present",  data: seriesData.patientsOnly }, 
+        { name: "Guardian present",  data: seriesData.guardiansOnly },
+        { name: "Both patient and guardian present", data: seriesData.both }
+      ]
     },
-    sortReadyForRendering(patients, guardians, both_patient_and_guardian){
-       let visit_dates = this.visit_dates;
-
-       for(let i = 0; i < visit_dates.length; i++){
-        this.sorted_data_patients.push([new Date(visit_dates[i]).getTime(),
-          patients[visit_dates[i]]]);
-
-        this.sorted_data_guardians.push([new Date(visit_dates[i]).getTime(),
-          guardians[visit_dates[i]]]);
-
-        this.sorted_data_both_patient_guardians.push([new Date(visit_dates[i]).getTime(),
-          both_patient_and_guardian[visit_dates[i]]]);
+    buildSummaryData (patients, guardians, both) {
+      this.patientVisits = Object.values(patients).reduce((acc, curr) => acc.concat(curr), [])
+      this.guardianVisits = Object.values(guardians).reduce((acc, curr) => acc.concat(curr), [])
+      this.totalAttendance = [
+        ...this.patientVisits,
+        ...this.guardianVisits,
+        ...Object.values(both).reduce((acc, curr) => acc.concat(curr), [])
+      ];
+    },
+    checkResult(visits){
+      let patientsOnly = {};
+      let guardiansOnly = {};
+      let bothPatientsAndGuardians = {};
+      let seriesData = {
+        patientsOnly: [],
+        guardiansOnly: [],
+        both: []
       }
-      //document.getElementById('spinner').style = 'display: none;';
-      //document.getElementById('report-cover').style = 'display: none;'
+
+      for(let date in visits){
+        patientsOnly[date] = [];
+        guardiansOnly[date] = [];
+        bothPatientsAndGuardians[date] = [];
+        Object.entries(visits[date]).forEach(([patientId, {guardian_present, patient_present}]) => {
+          if(guardian_present && patient_present) bothPatientsAndGuardians[date].push({patientId, date})
+          else if (patient_present) patientsOnly[date].push({patientId, date})
+          else if (guardian_present) guardiansOnly[date].push({patientId, date})
+        })
+        seriesData.patientsOnly.push([new Date(date).getTime(), patientsOnly[date].length])
+        seriesData.guardiansOnly.push([new Date(date).getTime(), guardiansOnly[date].length])
+        seriesData.both.push([new Date(date).getTime(), bothPatientsAndGuardians[date].length])
+      }
+      this.updateChart(seriesData);
+      this.buildSummaryData(patientsOnly, guardiansOnly, bothPatientsAndGuardians);
     },
     redraw(){
+    },
+    getARVNumber (patientIdentifiers = []) {
+      const arvNumber = patientIdentifiers.find(id => id.identifier_type == 4)
+      return arvNumber ? arvNumber.identifier : ""
+    },
+    toDateString (date) {
+      return moment(date).format("YYYY/MMM/DD");
+    },
+    getPatientDetails (patientId) {
+      return ApiClient.get(`patients/${patientId}`, {}, {})
+        .then(async (res) => {
+          if (res.status === 200) {
+            const patient = await res.json()
+            return {
+              arv_number: this.getARVNumber(patient.patient_identifiers),
+              first_name: patient.person?.names[0]?.given_name,
+              last_name: patient.person?.names[0]?.family_name,
+              birthdate: this.toDateString(patient.person?.birthdate),
+              gender: formatGender(patient.person?.gender),
+            }
+          }
+          return null
+        })
+    },
+    onSummaryDrill (patients, title = "Drill Down Clients") {
+      if(patients.length) {
+        this.drillTitle = title
+        this.$bvModal.show("modal-1");
+        this.drillClients = [];
+        patients.forEach(async ({patientId, date}) => {
+          const patient = await this.getPatientDetails(patientId)
+          if(patient) this.drillClients.push({...patient, visit_date: this.toDateString(date) })
+        });
+      }
     }
   },
   data () {
     return {
       page_title: 'Visit stats',
+      drillTitle: '',
       reportLoading: false,
-      sorted_data_patients: [],
-      sorted_data_guardians: [],
-      sorted_data_both_patient_guardians: [],
-      visit_dates: [],
+      perPage: 10,
+      currentPage: 1,
+      drillClients: [],
+      patientVisits: [],
+      guardianVisits: [],
+      totalAttendance: [],
       chartOptions: {
         chart: {
           zoomType: 'x'
@@ -202,23 +234,6 @@ export default {
             }
           }
         },
-        /*allowPointSelect: true,
-        point: {
-          events:{
-            click: ({e}) => { 
-              try {
-                let valueDescription = e.target.accessibility.valueDescription;
-                //showClients(e.target.series.name,
-                //e.target.accessibility.valiceDescription);
-                console.log(valueDescription)
-              }catch(x){
-                let show_message = `Something went wrong. 
-                Please try changing the date parameters or <b>Zooming in</b>.`;
-                console.log(show_message);
-              }
-            }
-          }
-        },*/
         area: {
           fillColor: {
             linearGradient: {
@@ -226,11 +241,7 @@ export default {
               y1: 0,
               x2: 0,
               y2: 1
-            }/*,
-            stops: [
-              [0, this.highcharts.getOptions().colors[0]],
-              [1, this.highcharts.color(this.highcharts.getOptions().colors[0]).setOpacity(0).get('rgba')]
-            ]*/
+            }
           },
           marker: {
             radius: 2
